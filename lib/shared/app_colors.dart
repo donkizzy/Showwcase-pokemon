46491848ff97ca2f1bfa2da28a37fc029b9efef7
amosgodwin500@gmail.com <HackMeNot123>
2022-01-28 20:28:22 +0100

    added app colors

---
+++ lib/shared/app_colors.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:flutter/material.dart';
+
+const Color ceruleanBlue = Color(0xff3558CD);
+const Color mirageBlue = Color(0xff161A33);
+const Color doveGrey = Color(0xff6B6B6B);
+const Color mecuryGrey = Color(0xffE8E8E8);
+const Color cerisePink = Color(0xffCD2873);
+const Color goldTipsYellow = Color(0xffEEC218);
+const Color periwinklePurple = Color(0xffD5DEFF);
